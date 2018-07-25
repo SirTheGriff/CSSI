241f19208e82855421f384e37fc53e1c3f5d7eb0
Justin Baird <sirthegriff@yahoo.com>
2018-07-25 13:47:18 +0000

    working

--- public/js/app.js
+++ public/js/app.js
@@ -1,27 +1,31 @@
 
 $('button').click(function() {
         alert($(this).attr("data-id"));
+        var id = $(this).attr("data-id");
     $.ajax({
         type: "POST",
+        traditional: true,
         dataType: "json",
+        context: id,
+        data: id,
         url: "/listusers",
-        success: function(result) {
+        success: function(id) {
 
             // $(".details_button span").empty();
-            $("#enterfname").html(result.firstname);
-            $("#enterlname").html(result.lastname);
-            $("#entercompany").html(result.companyname);
-            $("#entertitle").html(result.title);
-            $("#enteradd").html(result.address);
-            $("#enterzip").html(result.zip);
-            $("#entercity").html(result.city);
-            $("#enterstate").html(result.state);
-            $("#entercountry").html(result.country);
-            $("#enterphone").html(result.phone);
-            $("#enteremail").html(result.email);
-            $("#enterweb").html(result.web);
-            $("#entersys").html(result.system);
-            console.log(result);
+            $("#enterfname").html(id.firstname);
+            $("#enterlname").html(id.lastname);
+            $("#entercompany").html(id.companyname);
+            $("#entertitle").html(id.title);
+            $("#enteradd").html(id.address);
+            $("#enterzip").html(id.zip);
+            $("#entercity").html(id.city);
+            $("#enterstate").html(id.state);
+            $("#entercountry").html(id.country);
+            $("#enterphone").html(id.phone);
+            $("#enteremail").html(id.email);
+            $("#enterweb").html(id.web);
+            $("#entersys").html(id.system);
+            console.log(id);
         },
         error: function(err) {
            $("#userdetails span").html("<strong> Error </strong>");
